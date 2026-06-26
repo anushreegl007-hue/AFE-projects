514a3788d6ddce57da477c2e9921ad44a8a48df3
anushreegl007-hue <anushreegl007@gmail.com>
2026-06-26 15:58:25 +0530

    Initial Portfolio website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+alert("Welcome to My Portfolio Website!");
+
+// Button click event
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    alert("Resume will be added soon!");
+});
